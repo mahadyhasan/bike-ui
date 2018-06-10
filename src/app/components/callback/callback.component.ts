f1286d53447d56fa03fe286a93e0b90d15efacee
mahadyhasan <mahady.hasan@hotmail.com>
2018-06-10 19:14:14 +0100

    with security updated

--- src/app/components/callback/callback.component.ts
+++ src/app/components/callback/callback.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-callback',
-  template: `
-    <p>
-      callback works!
-    </p>
-  `,
+  templateUrl: './callback.component.html',
   styleUrls: ['./callback.component.css']
 })
 export class CallbackComponent implements OnInit {
